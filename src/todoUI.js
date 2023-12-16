62df35d132b9c66f4e4f6f4c64838cca0f35982b
Arturmoura123 <artur.moura.07@gmail.com>
2023-12-16 17:59:12 +0000

    now user needs to fill all the camps for the todo to be validate. Display warnings of missing part. dueDate cannot be in the past

--- src/todoUI.js
+++ src/todoUI.js
@@ -4,24 +4,53 @@ import {toDo, toDoList} from "./todoLogic.js"
 const thetoDoList = new toDoList() 
 
 export function setupTodoUI() {
-    document.addEventListener("DOMContentLoaded", () => {
+
 
         const addbutton = document.querySelector("#add-toDo");
 
         addbutton.addEventListener("click", () => {
-            const value = document.querySelector("#todo-title").value;
+            const title = document.querySelector("#todo-title").value;
             const description = document.querySelector("#todo-description").value;
-            const date = document.querySelector("#todo-dueDate").value;
+            const dueDate = document.querySelector("#todo-dueDate").value;
             const priority = document.querySelector("#todo-priority").value;      
 
             addnewtoDo(title, description, dueDate, priority)
         })
-    })
-}
+    }
+
 
 
 function addnewtoDo (title, description, dueDate, priority) {
-    thetoDoList.addToDo(title, description, dueDate, priority)
+    let warnings = []
+    let isFieldMissing = false
+
+    if (!title.trim()) {
+        isFieldMissing = true;
+        warnings.push("You are missing the title")
+    }
+
+    if (!description.trim()) {
+        isFieldMissing = true;
+        warnings.push("You are missing the description")
+    }
+
+    const today = new Date();
+    if (!dueDate) {
+        isFieldMissing = true;
+        warnings.push("You are missing the date")
+    } else if (today > new Date(dueDate)) {
+        isFieldMissing = true;
+        warnings.push("The due date cannot be in the past")
+    }
+
+    if (isFieldMissing === true) {
+        alert(warnings.join("\n"));
+        return;
+    } 
+    else {
+        thetoDoList.addToDo(title, description, dueDate, priority)
+        updateUIDisplay();  
+    }
 }
 
 
@@ -34,10 +63,10 @@ function updateUIDisplay() {
         tododiv.classList.add("todo-item");
 
         tododiv.innerHTML = `
-        <h3>Title: ${title}</h3>
-        <p>Description: ${description}</p>
-        <p>Date: ${dueDate}</p>
-        <p> ${priority} </p>
+        <h3>Title: ${todo.title}</h3>
+        <p>Description: ${todo.description}</p>
+        <p>Date: ${todo.dueDate}</p>
+        <p>Priority: ${todo.priority} </p>
         `;
         
         const deletebtn = document.createElement("button")
@@ -48,7 +77,7 @@ function updateUIDisplay() {
             updateUIDisplay();
         });
 
-        tododiv.appendChild(deleteButton)
+        tododiv.appendChild(deletebtn)
         big_container.appendChild(tododiv)
         
     });
