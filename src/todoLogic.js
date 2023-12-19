eee62b80ced7042b1a0b2effaf889d5cac774809
Arturmoura123 <artur.moura.07@gmail.com>
2023-12-19 11:33:48 +0000

    add a color system - the todo will recognize the user's priority and have a different background color for each

--- src/todoLogic.js
+++ src/todoLogic.js
@@ -1,35 +1,29 @@
 export class toDo {
-    constructor(title, description, tag, dueDate, priority) {
-        this.title = title;
-        this.description = description;
-        this.tag = tag
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
+  constructor(title, description, tag, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.tag = tag;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
 }
-
 
 export class toDoList {
-    constructor() {
-        this.todos = []
-    }
+  constructor() {
+    this.todos = [];
+  }
 
+  addToDo(title, description, tag, dueDate, priority) {
+    const new_todo = new toDo(title, description, tag, dueDate, priority);
+    this.todos.push(new_todo);
+    return new_todo;
+  }
 
-    addToDo(title, description, tag, dueDate, priority){
-        const new_todo = new toDo(title, description, tag, dueDate, priority);
-        this.todos.push(new_todo)
-        return new_todo
-    }
+  deleteToDo(title) {
+    this.todos = this.todos.filter((todo) => todo.title !== title);
+  }
 
-
-    deleteToDo(title) {
-        this.todos = this.todos.filter(todo => todo.title !== title);
-    }
-
-
-    getTodo(title) {
-        return this.todos.find(todo => todo.title === title);
-    }
+  getTodo(title) {
+    return this.todos.find((todo) => todo.title === title);
+  }
 }
-
-
