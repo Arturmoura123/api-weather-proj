e5394ee9e65554518f139fa7a80d6089501caef3
Arturmoura123 <artur.moura.07@gmail.com>
2023-12-22 18:01:24 +0000

    fetching weatherapi and sorting wanted data to index.html. Basic html structure with city input place, giving the user the posibility to change city and get insta data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import "./style.css"
-import "./global.css"
-
-
-console.log("Hello World")
+const api_key = "446f214866104b408e6193523232012"
 
 const container = document.querySelector("#content")
-
-const main = document.createElement("main")
-main.innerHTML = "<h1>This is the meteo for the city<h1>"
-
-const city = document.createElement("div")
-city.innerHTML = "<p>This city has 24 degrees</p>"
+const data_content = document.querySelector(".data-content")
 
 
-main.appendChild(city)
-container.appendChild(main)
+async function getWeather(searchTerm = "London") {
+  try {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${searchTerm}&aqi=no`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const meteoData = await response.json();
+    const cityName = meteoData.location.name
+    data_content.innerHTML = `<p>City name: ${cityName}<br>Temperature: ${meteoData.current.temp_c}°C<br>Condition: ${meteoData.current.condition.text}</p>`;
+  } catch (e) {
+    console.error("Fetch error: " + e.message);
+    data_content.innerHTML = `<p>Error loading data</p>`;
+  }
+}
+
+
+function changeCityName() {
+  const input = document.querySelector("#cityInput")
+  const inputData = input.value
+  getWeather(inputData)
+}
+
+
+const changeCityButton = document.querySelector("#searchButton")
+changeCityButton.addEventListener("click", changeCityName)
+
+
+
+
